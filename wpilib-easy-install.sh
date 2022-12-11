86c07dc2c5864be2067562bf1555697b7babf1d6
MMarchjr <103056947+mmarchjr@users.noreply.github.com>
2022-12-11 23:52:50 +0000

    Update wpilib-easy-install.sh

--- wpilib-easy-install.sh
+++ wpilib-easy-install.sh
@@ -2,5 +2,5 @@ curl -s https://api.github.com/repos/wpilibsuite/allwpilib/releases/latest \
 | grep "browser_download_url.*tar.gz" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -qic - -O wpilib.tar.gz
+| wget -qi - -O wpilib.tar.gz
 
