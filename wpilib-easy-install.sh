ab0364d05136730fa80a3c088b968b77c72dfa44
MMarchjr <103056947+mmarchjr@users.noreply.github.com>
2022-12-11 23:40:02 +0000

    Update wpilib-easy-install.sh

--- wpilib-easy-install.sh
+++ wpilib-easy-install.sh
@@ -2,6 +2,5 @@ curl -s https://api.github.com/repos/jgm/pandoc/releases/latest \
 | grep "browser_download_url.*tar.gz" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -qi - \
-| tar -xvf \ 
-| cd -
+| wget -qi - 
+
