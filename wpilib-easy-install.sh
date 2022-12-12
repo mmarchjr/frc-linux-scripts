110f99b7b63b2074204bd96b3315a98fd9aaabd3
MMarchjr <103056947+mmarchjr@users.noreply.github.com>
2022-12-12 00:32:01 +0000

    Update wpilib-easy-install.sh

--- wpilib-easy-install.sh
+++ wpilib-easy-install.sh
@@ -6,3 +6,4 @@ curl -s https://api.github.com/repos/wpilibsuite/allwpilib/releases/latest \
  tar -xf wpilib.tar.gz
  find / -maxdepth 3 -type d -name "WPILib*" 2>/dev/null \
 | cd 
+|./WPILibInstaller
